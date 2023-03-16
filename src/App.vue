d914e337c74e3730987c69337f0ee97f4333d35b
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-03-16 09:24:40 -0500

    study all structure and view data() and create()

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from "./components/Search.vue";
+import Todos from "./components/Todos.vue";
+import TodoApp from "./components/TodoApp.vue";
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div id="header">
+      <Search msg="Hello word" />
+    </div>
+    <div id="main-container"></div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+export default {
+  name: "App",
+  components: {
+    Search,
+    Todos,
+    TodoApp,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 0,
+          title: "comprar la cena",
+          completed: false,
+        },
+      ],
+      copyTodos: [],
+    };
+  },
+  created() {
+    this.copyTodos = [...this.todos];
+  },
+};
+</script>
+<style scoped></style>
